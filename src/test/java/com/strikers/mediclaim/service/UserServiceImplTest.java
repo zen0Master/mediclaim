bfe3339fa151dd106cd42d3363053d80fbdd0823
User1 <sujalshaikhsk@gmail.com>
2019-12-11 17:01:45 +0530

    test cases

---
+++ src/test/java/com/strikers/mediclaim/service/UserServiceImplTest.java
@@ -0,0 +1,64 @@
+package com.strikers.mediclaim.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.google.common.base.Optional;
+import com.strikers.mediclaim.entity.PolicyClaim;
+import com.strikers.mediclaim.entity.User;
+import com.strikers.mediclaim.repository.PolicyClaimRepository;
+import com.strikers.mediclaim.repository.UserRepository;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class UserServiceImplTest {
+
+	@InjectMocks
+	UserServiceImpl userServiceImpl;
+
+	@Mock
+	UserRepository userRepository;
+	@Mock
+	PolicyClaimRepository policyClaimRepository;
+
+	User user = new User();
+	PolicyClaim policyClaim = new PolicyClaim();
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		user.setUsername("Sujal");
+		user.setPassword("sujal@123");
+		user.setStatus("approved");
+		user.setUserId(1);
+
+	}
+
+	@Test
+	public void testLogin() {
+		Mockito.when(userRepository.findByUsernameAndPasswordAndStatus("Sujal", "sujal@123", "Success"))
+				.thenReturn(user);
+		userServiceImpl.userLogin(user);
+		assertEquals("approved", user.getStatus());
+
+	}
+
+	@Test
+	public void testPolicyClaim() {
+		List<PolicyClaim> policyClaimList = new ArrayList<>();
+		policyClaimList.add(policyClaim);
+
+		Mockito.when(policyClaimRepository.findAll()).thenReturn(policyClaimList);
+
+	}
+}
