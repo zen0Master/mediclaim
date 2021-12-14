18cc12e1a8327e597022c180a77b5137c150507f
nikhilbandagi <91196776+nikhilbandagi@users.noreply.github.com>
2021-12-14 14:40:04 +0530

    Update SwaggerConfig.java

--- src/main/java/com/strikers/mediclaim/config/SwaggerConfig.java
+++ src/main/java/com/strikers/mediclaim/config/SwaggerConfig.java
@@ -11,13 +11,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
-    @Bean
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+
+	}
+
 }
