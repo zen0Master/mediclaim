90fce3fdcfee86c209c84a5f1e61dc7b3d2dee9a
nikhilbandagi <91196776+nikhilbandagi@users.noreply.github.com>
2021-12-14 14:39:17 +0530

    Update SwaggerConfig.java

--- src/main/java/com/strikers/mediclaim/config/SwaggerConfig.java
+++ src/main/java/com/strikers/mediclaim/config/SwaggerConfig.java
@@ -11,24 +11,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
-public class SwaggerConfig extends WebMvcConfigurerAdapter {                                    
+public class SpringFoxConfig {
     @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
-
-   @Override
-   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-       registry.addResourceHandler("swagger-ui.html")
-       .addResourceLocations("classpath:/META-INF/resources/");
-
-       registry.addResourceHandler("/webjars/**")
-       .addResourceLocations("classpath:/META-INF/resources/webjars/");
-   }
-
 }
